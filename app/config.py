3f707b452a1ca7501fdad53ee9cca6eb3249dde8
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-14 03:38:40 +0300

    Adds thearticle base url

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ class Config:
     General configuration parent class
     '''
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
+    ARTICLE_API_BASE_URL = 'https://newsapi.org/v2/everything?{}&apiKey={}'
 
 
 
