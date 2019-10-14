b60aa500cea4c27571bd27a247c2e80cfc6d71b8
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-14 03:22:33 +0300

    Adds a macro template that helps to reduce redundancy and make our code dry by putting the all tha loops in it and removing them  from the other tempaltes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
 # Initializing application
 app = Flask(__name__,instance_relative_config = True)
@@ -10,4 +11,7 @@ app = Flask(__name__,instance_relative_config = True)
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
 
+bootstrap = Bootstrap(app)
+
 from app import views
+from app import error
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 
 from  flask import render_template
 from app import app
+from .request import get_news
 
 # Views
 @app.route('/')
@@ -9,7 +10,9 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    # Getting news
+    my_news = get_news()
+    return render_template('index.html',news = my_news)
 
 @app.route('/news/<id>')
 def article(movie_id):
@@ -17,4 +20,5 @@ def article(movie_id):
     '''
     View movie page function that returns the movie details page and its data
     '''
+    
     return render_template('article.html',id = movie_id)
